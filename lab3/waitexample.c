5233b57d6301656fd89a525fec085789e1ee154d
Kimgitae <rlarlxo2323@naver.com>
2021-12-12 05:16:59 +0000

    add file lab3 & mkdir lab4

---
+++ lab3/waitexample.c
@@ -0,0 +1,29 @@
+/* waitprocess.c */
+/* wait example */
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+	pid_t pid;
+	int status, exit_status;
+	
+	if ((pid = fork()) < 0)
+		perror ("fork failed");
+	if (pid == 0) {
+		sleep(4); /* ¼öÇàÀ» 4ÃÊ µ¿¾È Áß´Ü */
+		exit(5);
+	}
+
+	if ((pid = wait(&status)) == -1) {
+		perror("wait failed");
+		exit(2);
+	}
+	if (WIFEXITED(status)) {
+		exit_status = WEXITSTATUS(status);
+		printf("Exit status from %d was %d\n", pid, exit_status);
+	}
+	exit(0);
+}
